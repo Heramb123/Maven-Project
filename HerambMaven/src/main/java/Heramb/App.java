342fe3cd13ed25131595880848ad7cf43562cf4c
Heramb1307 <82360563+Heramb123@users.noreply.github.com>
2023-03-03 21:09:00 +0530

    Add files via upload

---
+++ HerambMaven/src/main/java/Heramb/App.java
@@ -0,0 +1,13 @@
+package Heramb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This Is Heramb's First Maven Project" );
+    }
+}
